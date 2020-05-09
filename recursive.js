c45677013a71844a045073089e2eb48932cf9e7e
Xiaoqing-Lin <57362207+Xiaoqing-Lin@users.noreply.github.com>
2020-05-09 11:20:06 -0400

    revised comments on recursive code

--- recursive.js
+++ recursive.js
@@ -1,25 +1,35 @@
+// recursive solution
+
+// leetcode solution translated to Javascript
+// this solution swaps node values, does not manipulate next pointers
 const reverseBetween = function(head, m, n) {
   this.left = head;
-  this.stop = false;
+  this.continue = true;
   recurseAndReverse(head, m, n);
   return head;
 };
+
 const recurseAndReverse = function(right, m, n) {
   if (n === 1) return;
   right = right.next;
+
   if (m > 1) this.left = this.left.next;
   recurseAndReverse(right, m - 1, n - 1);
-  // In case both the pointers cross each other or become equal, we
-  // stop i.e. don't swap data any further. We are done reversing at this
-  // point.
-  if (this.left === right || right.next === this.left) this.stop = true;
-  // Until the boolean stop is false, swap data between the two pointers
-  if (!this.stop) {
-    const t = this.left.val;
+
+  // If the pointers cross each other or become equal,
+  // there is nothing else to swap
+  if (this.left === right || right.next === this.left) this.continue = false;
+
+  // Until the boolean stop is true, swap data between the two pointers
+  // When boolean is false, nothing happens and function resolves,
+  //    since this.stop = true, function keeps resolving/returning until stack is empty
+  if (this.continue) {
+    const temp = this.left.val;
     this.left.val = right.val;
-    right.val = t;
+    right.val = temp;
+
     // Move left one step to the right.
-    // The right pointer moves one step back via backtracking.
     this.left = this.left.next;
+    // The right pointer doesn't need to be moved, it is pointing at previous in the previous call
   }
 };
