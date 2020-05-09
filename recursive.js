d4287e061a07fffffbfc38e8e2a207b911fef222
Xiaoqing-Lin <57362207+Xiaoqing-Lin@users.noreply.github.com>
2020-05-09 12:40:45 -0400

    update: added another recursive solution

--- recursive.js
+++ recursive.js
@@ -1,35 +1,21 @@
 // recursive solution
-
-// leetcode solution translated to Javascript
-// this solution swaps node values, does not manipulate next pointers
-const reverseBetween = function(head, m, n) {
-  this.left = head;
-  this.continue = true;
-  recurseAndReverse(head, m, n);
-  return head;
-};
-
-const recurseAndReverse = function(right, m, n) {
-  if (n === 1) return;
-  right = right.next;
-
-  if (m > 1) this.left = this.left.next;
-  recurseAndReverse(right, m - 1, n - 1);
-
-  // If the pointers cross each other or become equal,
-  // there is nothing else to swap
-  if (this.left === right || right.next === this.left) this.continue = false;
-
-  // Until the boolean stop is true, swap data between the two pointers
-  // When boolean is false, nothing happens and function resolves,
-  //    since this.stop = true, function keeps resolving/returning until stack is empty
-  if (this.continue) {
-    const temp = this.left.val;
-    this.left.val = right.val;
-    right.val = temp;
-
-    // Move left one step to the right.
-    this.left = this.left.next;
-    // The right pointer doesn't need to be moved, it is pointing at previous in the previous call
+function reverseBetween(head, m, n) {
+  if (m === n || head === null) return head;
+  if (n === 1) {
+    this.con = head.next;
+    return head;
   }
-};
+  const r = reverseBetween(head.next, m - 1, n - 1);
+  if (m <= 1) {
+    head.next.next = head;
+    head.next = null;
+    if (m === 1) {
+      head.next = this.con;
+    }
+    return r;
+  }
+  if (m === 2) {
+    head.next = r;
+  }
+  return head;
+}
